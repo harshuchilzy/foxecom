007f73b93d82ec5610bf6a9f548e3861484f8cef
harshu-nk <harshu.nk62@gmail.com>
2025-06-04 13:41:23 +0530

    Lunar integrated

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Lunar\Base\Traits\LunarUser;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, LunarUser;
 
     /**
      * The attributes that are mass assignable.
