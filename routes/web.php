13b70976d992a7dc89d049662dda1bce213f3b38
DayZ Solutions <dayzsolutions@DayZs-Mac-mini.local>
2025-06-08 10:33:54 +0530

    single product page with responsive

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
+Route::get('/single-product', function () {
+    return view('singleproduct');
+});
+
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
