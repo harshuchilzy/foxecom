d89df463905375c114f5864ad00f34e6c7675e7d
hirushann <hirushan.perera2000@gmail.com>
2025-06-20 11:00:29 +0530

    missing styles fixed

--- resources/views/layouts/storefront.blade.php
+++ resources/views/layouts/storefront.blade.php
@@ -3,24 +3,11 @@
 
 <head>
     <meta charset="utf-8">
-    <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1"
-    >
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Foxecom</title>
-    <meta
-        name="description"
-        content="Foxecom Meta"
-    >
-    <link
-        href="{{ asset('css/app.css') }}"
-        rel="stylesheet"
-    >
-
-    <link
-        rel="icon"
-        href="{{ asset('images/blacklogo.png') }}"
-    >
+    <meta name="description" content="Foxecom Meta">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="icon" href="{{ asset('images/blacklogo.png') }}">
 
     @include('partials.head')
 </head>
