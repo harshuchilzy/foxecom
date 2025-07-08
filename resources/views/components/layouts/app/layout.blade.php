ac6c0b51e4b2bdcb7cec532d2e6255b5294622f7
LakshanRanasinghe <lakshan20b@gmail.com>
2025-07-08 11:30:39 +0530

    minor changers made

--- resources/views/components/layouts/app/layout.blade.php
+++ resources/views/components/layouts/app/layout.blade.php
@@ -11,6 +11,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+    <link rel="shortcut icon" href="{{ asset('images/blacklogo.png') }}">
 
     <!-- Styles / Scripts -->
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
