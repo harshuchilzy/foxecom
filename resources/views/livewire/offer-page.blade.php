8b836901d9590c7b6de9b87ae3c5cef49c128370
Hirushan Perera <122775119+hirushann@users.noreply.github.com>
2025-07-28 12:10:11 +0530

    final commits 3

--- resources/views/livewire/offer-page.blade.php
+++ resources/views/livewire/offer-page.blade.php
@@ -77,7 +77,7 @@
             <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="14" cy="14" r="14" fill="#D9D9D9" fill-opacity="0.5"/><path fill-rule="evenodd" clip-rule="evenodd" d="M11.343 13.9999L18.414 21.0709L17 22.4849L9.22199 14.7069C9.03451 14.5194 8.9292 14.2651 8.9292 13.9999C8.9292 13.7347 9.03451 13.4804 9.22199 13.2929L17 5.51489L18.414 6.92889L11.343 13.9999Z" fill="white"/></svg>
         </button>
     </div>
-    <div class="hidden lg:flex gap-5 items-center justify-between px-10 py-20 bg-no-repeat absolute top-[35%] right-0 offer-txt-bg">
+    <div class="hidden lg:flex gap-5 items-center justify-between px-10 py-26 bg-no-repeat absolute top-[35%] right-0 offer-txt-bg">
         <div class="claim-offer-label">
             <h2 class="text-[#FEE8FF] text-[32px] font-extrabold">{{ $discount->name }}</h2>
             <p class="font-bold text-white text-[24px]">{{ $displayText }}</p>
