d3f73ebb53275e45fc38ca7f7837f006dae82d77
harshu-nk <harshu.nk62@gmail.com>
2025-07-29 13:46:23 +0530

    Pricing fix

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -158,16 +158,27 @@ class ProductsPage extends Component
             ->with(['values.option', 'basePrices'])
             ->get();
 
+        $outerBoxQty = $product->attr('outer-box') ?? 1;
+
         $minBasePrice = null;
         $maxBasePrice = null;
         $minComparePrice = null;
         $maxComparePrice = null;
 
+        $prices = collect();
+
         foreach ($variations as $variant) {
+
             $base = $variant->basePrices->first();
+            // array_push($prices, $base);
+            $prices->push($base);
 
             $basePrice = $base?->price?->value;
             $comparePrice = $base?->compare_price?->value;
+
+            // $base->compare_price->value = $basePrice / $outerBoxQty;
+            // $base->compare_price->value = $comparePrice / $outerBoxQty;
+
             $basePriceFormatted = $base?->price?->formatted;
             $comparePriceFormatted = $base?->compare_price?->formatted;
 
@@ -196,20 +207,63 @@ class ProductsPage extends Component
             }
         }
 
-        return [
-            'basePrice' => [
-                'min' => $minBasePrice !== null ? $minBasePrice : null,
-                'max' => $maxBasePrice !== null ? $maxBasePrice : null,
-                'min_formatted' => $minBasePriceFormatted !== null ? $minBasePriceFormatted : null,
-                'max_formatted' => $maxBasePriceFormatted !== null ? $maxBasePriceFormatted : null,
-            ],
-            'comparePrice' => [
-                'min' => $minComparePrice !== null ? $minComparePrice : null,
-                'max' => $maxComparePrice !== null ? $maxComparePrice : null,
-                'min_formatted' => $minComparePriceFormatted !== null ? $minComparePriceFormatted : null,
-                'max_formatted' => $maxComparePriceFormatted !== null ? $maxComparePriceFormatted : null,
-            ],
-        ];
+        // $outerBoxQty = 5;
+        $outerBoxQty = $product->attr('outer-box') ?? 1;
+
+        $pricesWithEffectivePrice = $prices->map(function ($item) use ($outerBoxQty) {
+            $effectivePrice = ($item->compare_price->value ?? 0) > 0
+                ? $item->compare_price->value
+                : $item->price->value;
+
+            $item->per_unit_price = $effectivePrice / $outerBoxQty;
+            // Log::info($item->per_unit_price);
+
+            return $item;
+        });
+
+        $lowest = $pricesWithEffectivePrice->sortBy('per_unit_price')->first();
+        $highest = $pricesWithEffectivePrice->sortByDesc('per_unit_price')->first();
+
+        if($lowest->compare_price->value > 0){
+            $lowest->compare_price->value = $lowest->compare_price->value / $outerBoxQty;
+        }else{
+            $lowest->price->value = $lowest->price->value / $outerBoxQty;
+        }
+
+         if($highest->compare_price->value > 0){
+            $highest->compare_price->value = $highest->compare_price->value / $outerBoxQty;
+        }else{
+            $highest->price->value = $highest->price->value / $outerBoxQty;
+        }
+
+        // Log::info('Prices');
+
+        // Log::info($lowest);
+        // Log::info($highest);
+
+        // Log::info('End Prices');
+
+        $finalPrice = $lowest->price->formatted . ' - ' . $highest->price->formatted; 
+        return array(
+            'discount' => $lowest->compare_price->value ?? 0,
+            'price' => $finalPrice
+        );
+        
+
+        // return [
+        //     'basePrice' => [
+        //         'min' => $minBasePrice !== null ? $minBasePrice : null,
+        //         'max' => $maxBasePrice !== null ? $maxBasePrice : null,
+        //         'min_formatted' => $minBasePriceFormatted !== null ? $minBasePriceFormatted : null,
+        //         'max_formatted' => $maxBasePriceFormatted !== null ? $maxBasePriceFormatted : null,
+        //     ],
+        //     'comparePrice' => [
+        //         'min' => $minComparePrice !== null ? $minComparePrice : null,
+        //         'max' => $maxComparePrice !== null ? $maxComparePrice : null,
+        //         'min_formatted' => $minComparePriceFormatted !== null ? $minComparePriceFormatted : null,
+        //         'max_formatted' => $maxComparePriceFormatted !== null ? $maxComparePriceFormatted : null,
+        //     ],
+        // ];
     }
 
 
--- resources/views/components/product-cards/general-card.blade.php
+++ resources/views/components/product-cards/general-card.blade.php
@@ -1,52 +1,16 @@
 @props(['product'])
 
 @php
-    $price = $product->prices->first();
-    $comparePrice = $price->compare_price;
-
+    // $price = $product->prices->first();
     $priceRange = $this->getPriceRangeForProducts($product);
-
-    if ( $priceRange ) {
-        if ($priceRange['basePrice']['min'] == $priceRange['basePrice']['max']) {
-            $priceRangeFormatted = $priceRange['basePrice']['min_formatted'];
-        } else {
-            $priceRangeFormatted = $priceRange['basePrice']['min_formatted'] . ' - ' . $priceRange['basePrice']['max_formatted'];
-        }
-    } else {
-        $priceRangeFormatted = $price->price->formatted;
-    }
-
-    if ( $priceRange ) {
-        if ($priceRange['comparePrice']['min'] == $priceRange['comparePrice']['max']) {
-            $salePriceRangeFormatted = $priceRange['comparePrice']['min_formatted'];
-        } else {
-            $salePriceRangeFormatted = $priceRange['comparePrice']['min_formatted'] . ' - ' . $priceRange['comparePrice']['max_formatted'];
-        }
-    } else {
-        $salePriceRangeFormatted = $price->compare_price->formatted;
-    }
-
-    if ( $priceRange ) {
-        if ( $priceRange['comparePrice']['min'] > 0 && $priceRange['comparePrice']['max'] > 0) {
-            $hasDiscount = true;
-        } else {
-            $hasDiscount = false;
-        }
-    } else {
-        $hasDiscount = $price->compare_price->decimal > 0;
-    }
-
-    $saveAmount = $hasDiscount ? ($price->price->decimal - $comparePrice->decimal) : 0;
-    $discountPercentage = $hasDiscount ? round(($saveAmount / $price->price->decimal) * 100) : 0;
-    
 @endphp
 {{-- {{dd($product->variants()->first())}} --}}
 <div class="bg-white border border-[#008ECC] rounded-[16px] relative group hover:shadow-lg transition-shadow overflow-hidden shadow-[0px_4px_45px_0px_#00000020]">
-    @if($hasDiscount)
+    {{-- @if($hasDiscount)
         <div class="bg-[#008ECC] p-3 w-[25%] flex justify-center items-center absolute top-0 right-0 rounded-bl-[16px] rounded-tr-[16px] z-10">
             <p class="font-semibold text-white text-[14px]">{{ $discountPercentage }}% OFF</p>
         </div>
-    @endif
+    @endif --}}
 
     <div class="p-3 hover:cursor-pointer bg-[#F5F5F5] ">
         <a href="{{ route('product.view', $product->defaultUrl->slug) }}" wire:navigate>
@@ -68,18 +32,17 @@
 
         @if (auth()->check())
             <div class="flex items-center gap-2 flex-wrap">
-                @if($hasDiscount)
+    
+                @if(isset($priceRange['discount']) && !empty($priceRange['discount']))
                     <span class="text-gray-400 text-sm line-through">
-                        {{ $priceRangeFormatted }}
+                        {{ $priceRange['discount'] }}
                     </span>
-                @endif
-                @if($hasDiscount)
                     <span class="text-[#1275EE] text-[16px] md:text-lg font-semibold">
-                        {{ $salePriceRangeFormatted }} + VAT
+                        {{ $priceRange['price'] }} + VAT
                     </span>
                 @else
                     <span class="text-[#1275EE] text-[16px] md:text-lg font-semibold">
-                        {{ $priceRangeFormatted }} + VAT
+                        {{ $priceRange['price'] }} + VAT
                     </span>
                 @endif
             </div>
