94d53a7586aa64bf523a4a49db56055c2d00f1d7
LakshanRanasinghe <lakshan20b@gmail.com>
2025-07-29 15:19:24 +0530

    update add to cart wire function

--- app/Livewire/Components/AddToCart.php
+++ app/Livewire/Components/AddToCart.php
@@ -32,14 +32,16 @@ class AddToCart extends Component
     public function addToCart(): void
     {
         $this->validate();
-        $this->outer_box_qty = ($this->quantity) * ($this->purchasable->quantity_increment ?? 1);
+        $outerBoxQty = $this->purchasable?->product->attr('outer-box') ?? 1;
+        $this->outer_box_qty = ($this->quantity) * ($outerBoxQty);
         //Log::info(print_r($this->outer_box_qty, true));
         if ($this->purchasable->stock < $this->outer_box_qty) {
             $this->addError('quantity', 'The quantity exceeds the available stock.');
             return;
         }
 
-        $productVariantQuantityIncrement = $this->quantity * $this->purchasable?->quantity_increment;
+        // $productVariantQuantityIncrement = $this->quantity * $this->purchasable?->quantity_increment;
+        $productVariantQuantityIncrement = $this->quantity * $outerBoxQty;
 
         $existing = CartSession::lines()
             ->get()
