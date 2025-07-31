b0e2d05e4d6137b187cf376d1ee42c2416df16cc
harshu-nk <harshu.nk62@gmail.com>
2025-07-31 20:52:08 +0530

    home page discounts - brand image fix

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -282,6 +282,9 @@
 
                                             $brandId = $firstProduct->brand->id ?? null;
                                             $brandSlug = $this->getBrandSlug($brandId);
+
+                                            $brandImageUrl = $discount->discountables->first()?->discountable->brand->getMedia('*')[0]->getUrl();
+
                                         @endphp
 
                                         @if (!empty($brandImageUrl) && !empty($brandSlug))
