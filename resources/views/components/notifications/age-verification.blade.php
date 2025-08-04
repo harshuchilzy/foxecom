2a32d210feead079b3048270240924de0fa2bcb4
LakshanRanasinghe <lakshan20b@gmail.com>
2025-08-04 11:26:45 +0530

    add age verification buttons to popup

--- resources/views/components/notifications/age-verification.blade.php
+++ resources/views/components/notifications/age-verification.blade.php
@@ -1,8 +1,8 @@
 <div class="fixed inset-0 backdrop-blur-sm hidden items-center justify-center z-50" :class="{'hidden': !showPopup, 'flex': showPopup}" x-data="foxecomAgeVerification()" x-show="showPopup" x-init="init()">
-  <div class="bg-[radial-gradient(209.04%_209.04%_at_49.6%_50.56%,#0B0E2D_0%,#242E93_100%)] text-white p-14 rounded-xl shadow-xl w-full  max-w-sm sm:max-w-md md:max-w-2xl h-[780px] lg:h-[672px]  text-center space-y-6" >
+  <div class="bg-[radial-gradient(209.04%_209.04%_at_49.6%_50.56%,#0B0E2D_0%,#242E93_100%)] text-white p-14 rounded-xl shadow-xl w-full  max-w-sm sm:max-w-md md:max-w-2xl h-auto md:h-[780px] lg:h-[672px]  text-center space-y-6" >
     <!-- Logo -->
-    <div class="text-3xl font-bold tracking-wide mb-16">
-      <img src="{{ asset('images/logo-on-age-verification.png') }}" alt="Logo" class="mx-auto w-[140px]">
+    <div class="text-3xl font-bold tracking-wide md:mb-16 mb-8">
+      <img src="{{ asset('images/logo-on-age-verification.png') }}" alt="Logo" class="mx-auto md:w-[140px] w-[100px]">
     </div>
 
     <!-- Country dropdown -->
@@ -15,11 +15,11 @@
 
     <!-- Age Verification Text -->
     <div class="mb-10">
-      <h2 class="text-4xl font-semibold mb-5 font-inter">Age Verification Required</h2>
-      <p class="text-base font-semibold text-white font-inter mb-7">
+      <h2 class="md:text-4xl text-2xl font-semibold mb-5 font-inter">Age Verification Required</h2>
+      <p class="md:text-base text-sm font-semibold text-white font-inter mb-7">
         Certain products contain nicotine, which is a highly addictive substance. Sales are restricted to individuals 18 years and<br> older.
       </p>
-      <p class="text-xs font-medium font-inter text-gray-400">
+      <p class="md:text-xs text-[10px] font-medium font-inter text-gray-400">
         By entering this site, you agree to our Terms of<br> Use and acknowledge that you have read and<br> understood our Cookie Policy and Privacy Policy.
       </p>
     </div>
@@ -29,7 +29,7 @@
       <!-- Under 18 Button -->
       <button 
         type="button" 
-        class="w-40 px-4 py-3 rounded-lg text-center font-inter font-medium text-lg
+        class="w-40 px-4 py-3 rounded-lg text-center font-inter font-medium md:text-lg text-sm
               bg-gray-800 border border-gray-600 text-gray-300
               hover:bg-red-600 hover:text-white hover:border-red-700
               focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50
@@ -43,7 +43,7 @@
       <!-- Over 18 Button -->
       <button 
         type="button" 
-        class="w-40 px-4 py-3 rounded-lg text-center font-inter font-medium text-lg
+        class="w-40 px-4 py-3 rounded-lg text-center font-inter font-medium md:text-lg text-sm
               bg-blue-900 border border-blue-700 text-blue-100
               hover:bg-green-600 hover:text-white hover:border-green-700
               focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50
