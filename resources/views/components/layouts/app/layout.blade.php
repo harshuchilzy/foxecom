b0febdfb354adc067f9fceeebea7f346d004a20b
LakshanRanasinghe <lakshan20b@gmail.com>
2025-10-14 12:57:57 +0530

    add changers to delivery policy

--- resources/views/components/layouts/app/layout.blade.php
+++ resources/views/components/layouts/app/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>FOXERGO</title>
+    <title>Foxergo</title>
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
